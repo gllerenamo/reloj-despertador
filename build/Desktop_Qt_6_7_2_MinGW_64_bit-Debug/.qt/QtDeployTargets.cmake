bb1c619bee2f966ce31e1eda15f8eff8df8808c2
gllerenamo <gllerenamo@unsa.edu.pe>
2024-10-08 22:21:42 -0500

    eliminacion de la carpeta build

--- build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_reloj-despertador_FILE C:/Users/Gabriel/Documents/UNSA 2024-B/TO/reloj-despertador/reloj-despertador/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/reloj-despertador.exe)
-set(__QT_DEPLOY_TARGET_reloj-despertador_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_reloj-despertador_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
